d0c2dabc8c74f440ea108ede021984439580a0a6
vikram <vikram.engg539@gmail.com>
2022-07-01 18:47:01 +0530

    Swiss project added

--- spin-aviation/index.php
+++ spin-aviation/index.php
@@ -7,15 +7,16 @@
     <title>SBCM - AG</title>
     <link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet">
     <script src="bootstrap5/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="css/style.css"><link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <!-- Owl Stylesheets -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"><!-- Owl Stylesheets -->
     <link rel="stylesheet" href="owlcarousel/css/owl.carousel.min.css">
     <link rel="stylesheet" href="owlcarousel/css/owl.theme.default.min.css">
     
     <script  src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    
+    <!-- javascript -->
+    <script src="owlcarousel/js/owl.carousel.js"></script>
 </head>
 <body>
     <main>
@@ -30,8 +31,12 @@
                 </div>
             </div>
         </header>          
-        <?php include("assets/booking.php");  ?>
+        <?php include("assets/body-content.php");  ?>
+        <?php include("assets/popular-routes.php");  ?>
+        <?php include("assets/benefits.php");  ?>
+        
     </main>    
     <?php include("assets/footer.php");  ?>
+    
 </body>
 </html>
