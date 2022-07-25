2eb9cfca3cd9e50853c9dfafb89834af907e54ab
vikram539 <vikram.engg539@gmail.com>
2022-07-25 21:27:45 +0530

    spin aviation about us page

--- spin-aviation/assets/top-bar.php
+++ spin-aviation/assets/top-bar.php
@@ -3,22 +3,22 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="<?= $root ?>">
             <img src="images/logo.png" alt="Spin Aviation" class="img-fluid" />
         </a>
         <div class="collapse navbar-collapse justify-content-end" id="navbarToggler">
-            <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0  mx-auto">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0  mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="<?= $root ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About us</a>
+                    <a class="nav-link" href="<?= $root ?>/about-us.php">About us</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">booking</a>
                 </li>
-            </ul> -->
-            <ul class='d-flex m-0 p-0'>
+            </ul>
+            <ul class='d-flex m-0 p-0 list-unstyled'>
                 <li>
                     <div class="comm_btn">
                         <a href="" target="_blank" class="d-flex">request</a>
--- spin-aviation/index.php
+++ spin-aviation/index.php
@@ -1,30 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spin Aviation</title>
-    <link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet">
-    <script src="bootstrap5/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/style.css"><link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"><!-- Owl Stylesheets -->
-    <link rel="stylesheet" href="owlcarousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="owlcarousel/css/owl.theme.default.min.css">
-    
-    <script  src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    
-    <!-- javascript -->
-    <script src="owlcarousel/js/owl.carousel.js"></script>
+<?php 
+    include("assets/variables.php"); 
+    include("assets/header.php");  
+?>
 </head>
 <body>
     <main>
         <header class="top_header">
             <div class="topHeader_img_wrapper">
-                <div class="header_img">
-                    <img src="images/slider.jpg" alt="Spin Aviation" class="img-fluid" />
-                </div> 
+                <?php include("assets/slider-section.php");  ?>
                 <div class="menu_section">     
                     <?php include("assets/top-bar.php");  ?>
                     <?php include("assets/banner-text.php");  ?>
@@ -37,7 +20,6 @@
         <?php include("assets/our-aircrafts.php");  ?>
         
     </main>    
-    <?php include("assets/footer.php");  ?>
-    
+    <?php include("assets/footer.php");  ?>    
 </body>
 </html>
