410cc2f0053ddb036573aeaf40880ba61b9ea8a2
vikam539 <vikram.engg539@gmail.com>
2022-08-10 12:46:57 +0530

    Eight International Design

--- Eight-International/assets/footer.php
+++ Eight-International/assets/footer.php
@@ -1,8 +1,13 @@
-<footer>
+<footer class='marginTop_150'>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7 col-xl-7 col-xxl-7">
-                <div class="footerPolicy">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+                <div class="footerLogo">
+                    
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+                <div class="footerAddress">
                     
                 </div>
             </div>
--- Eight-International/index.php
+++ Eight-International/index.php
@@ -5,10 +5,37 @@
     <main>
         <header class="headerSection">
             <?php include("assets/navbar.php");  ?>
-            
-            <?php //include("assets/footer.php");  ?>
         </header>
-    </main>     
-    
+        <section>
+            <div class="container"
+                <div class="row">
+                    <div class="col-12">
+                        <div class="mainContent marginTop_150 text-center">
+                            <h2>HQ'd in Mauritius</h2>
+                            <h4>we in work with global clients for access to European securities. </h4>
+                            <p>Incorporating Swiss principles into our Financial approach empowers our clients with stability in their portfolios. </p>
+                        </div>
+                        <div class="mapWrapper mt-5">
+                            <div class="mapImage">
+                                <img src="<?= $images ?>Frame.svg" alt="<?= $name ?>" class='img-fluid' />
+
+                                <!-- animation -->
+                                <div class="locationWrap">
+                                    <div class="animateImg"></div>
+                                    <div class="fixedPathImg">                                        
+                                        <img src="<?= $images ?>location.png" alt="<?= $name ?>" class='img-fluid' />                                          
+                                        <img src="<?= $images ?>sign.png" alt="<?= $name ?>" class='img-fluid' />   
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include("assets/add-value.php");  ?>
+        <?php include("assets/our-clients.php");  ?>
+        <?php include("assets/footer.php");  ?>
+    </main> 
 </body>
 </html>
