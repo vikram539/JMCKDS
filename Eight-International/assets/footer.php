4a647237738a84e11f905f4327a0c9d3a4c0b877
vikram539 <vikram.engg539@gmail.com>
2022-08-11 20:32:33 +0530

    footer laayout added

--- Eight-International/assets/footer.php
+++ Eight-International/assets/footer.php
@@ -1,14 +1,20 @@
 <footer class='marginTop_150'>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+        <div class="row align-items-center">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
                 <div class="footerLogo">
                     <img src="<?= $images ?>footerLogo.png" alt="" class="img-fluid" />
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8">
                 <div class="footerAddress">
-                    
+                    <div class="footerInline">
+                        <h4><img src="<?= $images ?>mapMark.png" alt="" class="img-fluid" /><span>Eight International</span></h4>
+                        <p>C/o Anex Management Services Limited</p>
+                        <p>8th Floor - Ebene Tower</p>
+                        <p>52, Cybercity Ebene</p>
+                        <p>Rep. of Mauritius</p>
+                    </div>
                 </div>
             </div>
         </div>
