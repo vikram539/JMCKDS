b4eda563337b1f574b715ea241381ecad1916c85
vikram539 <vikram.engg539@gmail.com>
2022-09-16 21:00:47 +0530

    SIAM ecommerce product page detail..

--- SIAM/Ecommerce/detail.php
+++ SIAM/Ecommerce/detail.php
@@ -1,4 +1,5 @@
 <?php include("components/header.php"); ?>
+<link rel="stylesheet" href="<?=$root?>scripts/css/main.css">
 </head>
 <body class='bodyBg'>
     <main>
@@ -10,23 +11,45 @@
         <div class="productDetailWrap mtb">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 col-lg-7">
-                        <div class="secureSavingText">
-                            <h2 class='mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-                            <p class='mt-4'>Our inspiration is boundless, yet always grounded by a vision of timeless and luxurious beauty guiding everything we do.</p>
+                    <div class="col-12 col-lg-5">
+                        <div class="productThumbNail">
+                            <div class="show" href="<?=$images?>product/01.png">
+                                <img src="<?=$images?>product/01.png" id="show-img">
+                            </div>
+                            <div class="small-img">
+                                <img src="images/online_icon_right@2x.png" class="icon-left" alt="" id="prev-img">
+                                <div class="small-container">
+                                    <div id="small-img-roll">
+                                        <img src="<?=$images?>product/01.png" class="show-small-img" alt="">
+                                        <img src="<?=$images?>product/02.png" class="show-small-img" alt="">
+                                       
+                                        <img src="<?=$images?>product/01.png" class="show-small-img" alt="">
+                                        <img src="<?=$images?>product/02.png" class="show-small-img" alt="">
+                                       
+                                        <img src="<?=$images?>product/01.png" class="show-small-img" alt="">
+                                       
+                                    </div>
+                                </div>
+                                <img src="<?=$images?>online_icon_right@2x.png" class="icon-right" alt="" id="next-img">  
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-lg-5 d-lg-none">
-                        <img src="<?=$images?>gold.png" class='img-fluid' />
+                    <div class="col-12 col-lg-7">
+                        <div class="productDescription">
+                            <h2>250 Gram Gold Bar</h2>
+                            <p>Low premium 250 Gram investment grade gold bullion bar - brand of our choice.</p>
+                        </div>                        
                     </div>
                 </div>
             </div>
         </div> 
 
         <!-- Product List -->
-        <?php include("components/product-list.php") ?>
+        <?php //include("components/product-list.php") ?>
         
     </main>
     <?php include("components/footer.php") ?>
 </body>
 </html>
+  <script src="<?=$root?>scripts/zoom-image.js"></script>
+  <script src="<?=$root?>scripts/main.js"></script>
