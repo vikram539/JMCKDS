f58d899cb41b348d8d2b2b4522fe2679f0c4edb3
vikram539 <vikram.engg539@gmail.com>
2023-04-04 20:54:57 +0530

    blog

---
+++ Crypto-cronic/blog/blogFunction/function.php
@@ -0,0 +1,12 @@
+<?php
+    include("../webdesignbank-admin/php-function/connection.php");
+
+    class blogFun extends connection{
+
+        public function selectBlog(){
+           
+        }
+    }
+
+    $blogObj = new blogFun;
+?>
